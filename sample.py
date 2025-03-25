53b5d8f589121225cdb72adbf031f9fd1a6e309f
Sujith Kumar.M <155992233+sujith17kumar@users.noreply.github.com>
2025-03-25 14:43:23 +0530

    added a new no

--- sample.py
+++ sample.py
@@ -1,7 +1,8 @@
 num1 = 1
 num2 = 2
+num3 = 3
 
-sum = num1 + num2
+sum = num1 + num2 + num3
 
 
-print('The sum of {0} and {1} is {2}'.format(num1, num2, sum))
+print('The sum of {0} and {1} and {2} is {3}'.format(num1, num2, num3, sum))
